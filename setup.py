c39a098bed920169f8507ed43f14f07b394efa70
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-06-13 00:33:38 -0700

    give the app a proper name

--- setup.py
+++ setup.py
@@ -17,13 +17,11 @@ from setuptools import find_packages, setup
 # Setup ========================================================================
 
 setup(
-    name='flaskr',
-    version='1.0.0',
+    name='ucsd-bisb-unofficial',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'flask',
-    ],
+    install_requires=['flask']
 )
 
--- ucsd_bisb_unofficial/auth.py
+++ ucsd_bisb_unofficial/auth.py
@@ -16,7 +16,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from ucsd_bisb_unofficial.db import get_db
 
 
 
