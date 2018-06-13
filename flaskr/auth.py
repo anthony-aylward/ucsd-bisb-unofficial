54b7cde2e7b99f2974f1b7f70ffb0e3754ec6187
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-06-12 19:46:14 -0700

    more updates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -83,7 +83,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('blog.index'))
         
         flash(error)
     
@@ -107,7 +107,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 
 def login_required(view):
