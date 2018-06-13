8ed41c79d3d514959896d2a1aaef95b84dc9adc1
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-06-12 17:47:11 -0700

    customize

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -52,7 +52,8 @@ def create_app(test_config=None):
     
     from . import blog
     app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+    
+    app.add_url_rule('/', endpoint='login')
     
     return app
 
