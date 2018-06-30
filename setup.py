32a2d52610276ac6bcb74602875bc906a4dfc8e9
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-06-30 12:19:46 -0700

    advance version to 0.0.2

--- setup.py
+++ setup.py
@@ -18,10 +18,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='ucsd-bisb-unofficial',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=['flask']
 )
-
