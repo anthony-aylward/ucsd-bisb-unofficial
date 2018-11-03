5f02ef8ccdb771786db0dfaff6a61d019c16ddd3
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-11-03 14:27:19 -0700

    advance version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ucsd-bisb-unofficial',
-    version='0.0.14',
+    version='0.0.15',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
