51da455824190da41302ca88a714d19a6171df88
Anthony Aylward <anthonyaylward@Anthonys-MBP.attlocal.net>
2018-11-15 21:59:50 -0800

    restore

--- production_config/__init__.py
+++ production_config/__init__.py
@@ -34,7 +34,7 @@ SQLALCHEMY_DATABASE_URI = (
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
-MAIL_PORT = int(os.environ.get('MAIL_PORT')) or 465
+MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 MAIL_USERNAME = 'ucsd.bisb.unofficial'
