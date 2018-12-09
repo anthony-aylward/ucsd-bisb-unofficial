24764da567a9af7d8e840ac6121713f7f2f7b447
anthony-aylward <aaylward@eng.ucsd.edu>
2018-12-08 23:11:55 -0800

    debug

--- ucsd_bisb_unofficial/seminars.py
+++ ucsd_bisb_unofficial/seminars.py
@@ -30,7 +30,8 @@ from ucsd_bisb_unofficial.models import get_db, Post
 from ucsd_bisb_unofficial.principals import named_permission
 from ucsd_bisb_unofficial.blog import (
     get_post, construct_create_route, construct_update_route,
-    construct_delete_route, construct_detail_route
+    construct_delete_route, construct_detail_route, construct_comment_route,
+    construct_delete_comment_route
 )
 
 
