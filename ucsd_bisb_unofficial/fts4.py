16027c0d1f9a67cb68c65f0de817b2852dea4b8f
anthony-aylward <aaylward@eng.ucsd.edu>
2018-12-24 13:24:45 -0800

    make __searchable__ attribute work

--- ucsd_bisb_unofficial/fts4.py
+++ ucsd_bisb_unofficial/fts4.py
@@ -27,13 +27,25 @@ def validate_table_name(cursor, source_db_name, table_name):
 
 
 def validate_column_names(cursor, source_db_name, table_name, *column_names):
-    cursor.execute(f'select * from {source_db_name}.{table_name}')
+    cursor.execute(f'SELECT * FROM {source_db_name}.{table_name}')
     if not set(column_names) <= {tup[0] for tup in cursor.description}:
         raise ValueError('Invalid column names')
 
 
-def fts4_search(table, query, page, per_page):
-    __searchable__ = ('body',)
+def table_is_indexed(cursor, table_name):
+    return table_name in {
+        tup[0] for tup in cursor.execute(
+            "SELECT name FROM main.sqlite_master WHERE type='table'"
+        )
+    }
+
+
+def searchable_columns(cursor, table_name):
+    cursor.execute(f'SELECT * FROM main.{table_name}')
+    return tuple(tup[0] for tup in cursor.description)
+
+
+def fts4_index(table, id, searchable):
     c = current_app.fts4.cursor()
     source_db_name = 'source'
     c.execute(
@@ -44,26 +56,49 @@ def fts4_search(table, query, page, per_page):
         )
     )
     validate_table_name(c, source_db_name, table)
-    validate_column_names(c, source_db_name, table, *__searchable__)
-    c.executescript(f"""
-        CREATE VIRTUAL TABLE {table} USING fts4(
-            {', '.join(__searchable__)}
-        );
-
-        INSERT INTO {table}(docid, {', '.join(__searchable__)})
-        SELECT id, {', '.join(__searchable__)}
-        FROM {source_db_name}.{table};
-        """
+    validate_column_names(c, source_db_name, table, *searchable)
+    if not table_is_indexed(c, table):
+        c.execute(
+            f"CREATE VIRTUAL TABLE {table} USING fts4({', '.join(searchable)})"
+        )
+    c.execute(f'DELETE FROM {table} WHERE docid = ?', (id,))
+    c.execute(f"""
+        INSERT INTO {table}(docid, {', '.join(searchable)})
+        SELECT id, {', '.join(searchable)}
+        FROM {source_db_name}.{table}
+        WHERE id = ?
+        """,
+        (id,)
     )
-    q = (' OR '.join(f'{col}:{query}' for col in __searchable__),)
+    current_app.fts4.commit()
+    c.execute('DETACH ?', (source_db_name,))
+
+
+def fts4_delete(table, id):
+    c = current_app.fts4.cursor()
+    validate_table_name(c, 'main', table)
+    c.execute(f'DELETE FROM {table} WHERE id = ?', (id,))
+
+
+def fts4_search(table, query, page, per_page):
+    c = current_app.fts4.cursor()
+    source_db_name = 'source'
+    c.execute(
+        f'ATTACH ? AS ?',
+        (
+            current_app.config['SQLALCHEMY_DATABASE_URI'].split(':///')[1],
+            source_db_name
+        )
+    )
+    validate_table_name(c, source_db_name, table)
+    c.execute('DETACH ?', (source_db_name,))
+    searchable = searchable_columns(c, table)
     hits = tuple(
         tup[0] for tup in c.execute(
             f'SELECT docid FROM {table} WHERE {table} MATCH ?',
-            q
+            (' OR '.join(f'{col}:{query}' for col in searchable),)
         )
     )
-    c.execute(f'DROP TABLE {table}')
-    c.execute('DETACH ?', (source_db_name,))
     return {
         'hits': {
             'total': len(hits),
@@ -84,11 +119,13 @@ def search_results(search):
 def add_to_index(index, model):
     if not current_app.fts4:
         return
-
+    fts4_index(table=index, id=model.id, searchable=model.__searchable__)
+    
 
 def remove_from_index(index, model):
     if not current_app.fts4:
         return
+    fts4_delete(table=index, id=model.id)
 
 
 def query_index(index, query, page, per_page):
--- ucsd_bisb_unofficial/models.py
+++ ucsd_bisb_unofficial/models.py
@@ -29,7 +29,9 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from ucsd_bisb_unofficial.fts4 import query_index
+from ucsd_bisb_unofficial.fts4 import (
+    query_index, add_to_index, remove_from_index
+)
 
 
 
@@ -66,6 +68,7 @@ roles_whisper_users = db.Table(
 class SearchableMixin():
     @classmethod
     def search(cls, expression, page, per_page):
+        cls.reindex()
         ids, total = query_index(cls.__tablename__, expression, page, per_page)
         if total == 0:
             return cls.query.filter_by(id=0), 0
@@ -74,6 +77,32 @@ class SearchableMixin():
             when.append((ids[i], i))
         return cls.query.filter(cls.id.in_(ids)).order_by(
             db.case(when, value=cls.id).desc()), total
+
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+
+    @classmethod
+    def reindex(cls):
+        for obj in cls.query:
+            add_to_index(cls.__tablename__, obj)
 
 
 
@@ -321,7 +350,7 @@ class Post(SearchableMixin, db.Model):
     tag : str
     """
 
-    __searchable__ = ('body',)
+    __searchable__ = ('body', 'title')
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
@@ -549,3 +578,11 @@ def get_db():
     if 'db' not in g:
         g.db = db
     return g.db
+
+
+
+
+# Events =======================================================================
+
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
