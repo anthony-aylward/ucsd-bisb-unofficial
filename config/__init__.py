56aa3ca7e57daa93d01a4ae5dfe2e39e2f33dd14
anthony-aylward <aaylward@eng.ucsd.edu>
2019-01-02 12:18:01 -0800

    FTS_DATABASE config

--- config/__init__.py
+++ config/__init__.py
@@ -29,6 +29,7 @@ SQLALCHEMY_DATABASE_URI = (
     or 'sqlite:///' + os.path.join(basedir, 'app.db')
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+FTS_DATABASE = os.path.join(basedir, 'fts.db')
 MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
 MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
 MAIL_USE_TLS = True
@@ -45,15 +46,14 @@ APPROVED_EMAILS = []
 PRODUCTION_CONFIG_DATA = f'''
 import os
 import os.path
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 SECRET_KEY = os.environ.get('SECRET_KEY') or {os.urandom(16)}
 SQLALCHEMY_DATABASE_URI = (
     os.environ.get('DATABASE_URL')
     or 'sqlite:///' + os.path.join(basedir, 'app.db')
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+FTS_DATABASE = os.path.join(basedir, 'fts.db')
 MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
 MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
 MAIL_USE_TLS = False
