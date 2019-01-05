ed895512ec71e494345c93cfc59c7bd2855ae225
anthony-aylward <aaylward@eng.ucsd.edu>
2019-01-04 22:06:16 -0800

    tag in search results

--- ucsd_bisb_unofficial/search.py
+++ ucsd_bisb_unofficial/search.py
@@ -68,7 +68,8 @@ def search():
             'preview': post.body[:128] + (len(post.body) > 128) * '...',
             'detail_route': f'{post.tag}.detail',
             'document_url': post.document_url,
-            'image_url': post.image_url
+            'image_url': post.image_url,
+            'tag': post.tag
         }
         for post in posts
     )
