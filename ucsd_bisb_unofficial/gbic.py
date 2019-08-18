b1513714c4768f08f647ae887b3fb121934abafb
anthony-aylward <aaylward@eng.ucsd.edu>
2019-08-17 23:41:30 -0700

    try this

--- ucsd_bisb_unofficial/gbic.py
+++ ucsd_bisb_unofficial/gbic.py
@@ -19,13 +19,12 @@ bp : Blueprint
 
 # Imports ======================================================================
 
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint, render_template, current_app
 from flask_login import current_user, login_required
 from werkzeug.exceptions import abort
 
 from ucsd_bisb_unofficial.principals import named_permission
+from ucsd_bisb_unofficial.models import get_db, User
 
 
 
@@ -39,10 +38,33 @@ bp = Blueprint('gbic', __name__, url_prefix='/gbic')
 
 # Functions ====================================================================
 
+def is_registered(email):
+    return bool(User.query.filter_by(email=email).first())
+
+
+def signed_whisper_nda(email):
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return False
+    else:
+        return user.confidentiality_agreed
+
+
 @bp.route('/index')
 @login_required
 @named_permission.require(http_exception=403)
 def index():
     """Render the gbic index"""
+    status = {
+        office: {
+            'email': email,
+            'is_registered': is_registered(email),
+            'signed_whisper_nda': signed_whisper_nda(email)
+        }
+        for office, email in current_app.config['GBIC_EMAILS']
+    }
 
-    return render_template('gbic/index.html')
+    return render_template(
+        'gbic/index.html',
+        **status
+    )
