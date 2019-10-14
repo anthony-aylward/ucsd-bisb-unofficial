cbaf85f692b0ae961e5d38b4b8c93e9983318d61
anthony-aylward <aaylward@eng.ucsd.edu>
2019-10-13 17:33:37 -0700

    town hall section

--- ucsd_bisb_unofficial/townhall.py
+++ ucsd_bisb_unofficial/townhall.py
@@ -1,8 +1,8 @@
 #===============================================================================
-# gbic.py
+# townhall.py
 #===============================================================================
 
-"""GBIC blueprint
+"""committee blueprint
 
 Attributes
 ----------
@@ -24,54 +24,23 @@ from flask_login import current_user, login_required
 from werkzeug.exceptions import abort
 
 from ucsd_bisb_unofficial.principals import named_permission
-from ucsd_bisb_unofficial.models import get_db, User
 
 
 
 
 # Blueprint assignment =========================================================
 
-bp = Blueprint('gbic', __name__, url_prefix='/gbic')
+bp = Blueprint('townhall', __name__, url_prefix='/townhall')
 
 
 
 
 # Functions ====================================================================
 
-def is_registered(email):
-    return bool(User.query.filter_by(email=email).first())
-
-
-def signed_whisper_nda(email):
-    user = User.query.filter_by(email=email).first()
-    if user is None:
-        return False
-    else:
-        return user.confidentiality_agreed
-
-
-def status(email):
-    if not is_registered(email):
-        return ''
-    elif not signed_whisper_nda(email):
-        return '✔'
-    else:
-        return '✔✔'
-
-
 @bp.route('/index')
 @login_required
 @named_permission.require(http_exception=403)
 def index():
-    """Render the gbic index"""
+    """Render the town hall index"""
 
-    return render_template(
-        'gbic/index.html',
-        **{
-            office: {
-                'email': email,
-                'status': status(email)
-            }
-            for office, email in current_app.config['GBIC_EMAILS'].items()
-        }
-    )
+    return render_template('townhall/index.html')
