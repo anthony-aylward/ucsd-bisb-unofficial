9fd6897eb2ba29537ce6d44333da43c263d7b9e4
anthony-aylward <aaylward@eng.ucsd.edu>
2020-01-05 04:10:25 -0800

    debug

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ucsd-bisb-unofficial',
-    version='1.5.13',
+    version='1.5.14',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
