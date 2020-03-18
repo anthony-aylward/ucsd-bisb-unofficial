1cc22972dffe1ae075488d88d2bbe5bd572e961d
Anthony Aylward <aaylward@eng.ucsd.edu>
2020-03-18 15:20:32 -0700

    update td page

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ucsd-bisb-unofficial',
-    version='1.6.12',
+    version='1.6.13',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
